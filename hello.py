c740c9bc7d1de068d8eb9624565490bfa3b3780b
Ritista Shrestha <ritista99@gmail.com>
2024-07-15 21:42:42 +0545

    first commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+name = 'Ritista'
+age = 23
+phone = 980589000
+print("my name is {} age {} phone {}".format(name, age, phone))
+print(f"my name is {name} \n age {age} \n phone {phone}")
+
+import calendar
+print(calendar.month(2024,2))
+
+import datetime
+print(datetime.datetime.now())
+
+a = 10
+b = 19
+c = a
+print(a is b)
+print(a is c)
+print(a is not b)
+
+name = 'ritista'
+print('a' in name)
